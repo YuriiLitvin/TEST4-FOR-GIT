cbdd6b0c1bb12f88a0ff330c4b05b7cd7dfb2cc1
Yurii Litvin <yurijlitviniii@gmail.com>
2020-02-19 10:52:31 +0200

    All parsed  data become list, then go to the DBTables. I used ALTER TABLE  to add columns

--- TEST4 FOR GIT/Program.cs
+++ TEST4 FOR GIT/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using HtmlAgilityPack;
 using System.Data.SQLite;
 using System.IO;
+using System.Data;
 
 namespace TEST4_FOR_GIT
 {
@@ -34,13 +35,7 @@ namespace TEST4_FOR_GIT
         const string createQuery1 = @"CREATE TABLE IF NOT EXISTS
                                 [UkrOnline] (
                                 [ID] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                                [1] NVARCHAR (2048) NULL,
-                                [2] NVARCHAR (2048) NULL,
-                                [3] NVARCHAR (2048) NULL,
-                                [4] NVARCHAR (2048) NULL,
-                                [5] NVARCHAR (2048) NULL,
-                                [6] NVARCHAR (2048) NULL,
-                                [7] NVARCHAR (2048) NULL)";
+                                [1] NVARCHAR (2048) NULL)";
 
 
 
@@ -49,7 +44,7 @@ namespace TEST4_FOR_GIT
             Parser("http://www.ukr.net", "//article//section", "/*[position()<last()]//a", "UkrNet");
             Parser("http://www.ukr-online.com", "//td[1]/div[1]/div[@class ='lastblock']", "//a", "UkrOnline");
 
-            Console.ReadKey();
+            //Console.ReadKey();
         }
 
         public static void Parser(string url, string nodeCount, string nodeSelect, string tableName)
@@ -57,6 +52,8 @@ namespace TEST4_FOR_GIT
             HtmlWeb web = new HtmlWeb();
             HtmlDocument doc = web.Load(url);
             //
+            List<string> list1 = new List<string>();
+
             // determines amount of parse regions/SQLite columns
             int count = doc.DocumentNode.SelectNodes(nodeCount).Count;
             for (int i = 1; i <= count; i++)
@@ -66,25 +63,22 @@ namespace TEST4_FOR_GIT
 
                 //determines amount of urls in one parse region/SQLite rows
                 var htmlNodes = doc.DocumentNode.SelectNodes($"{nodeCount}[{i}]{nodeSelect}");
-                //int htmlNodeCount = htmlNodes.Count;
-
-                int countHtmlNodes = htmlNodes.Count;
-                //Console.WriteLine(countHtmlNodes);
-                int j = 1;
+                
+                //int j = 1;
                 foreach (var node in htmlNodes)
                 {
-                    
-                    //Console.WriteLine(node.Attributes["href"].Value);
-                    var nodeValue = node.Attributes["href"].Value;
-                    fillInTable(nodeValue, i, tableName, j);
-                    j++;
+                    string nodeValue = node.Attributes["href"].Value;
+                    list1.Add(nodeValue);
                 }
+                fillInTable(list1, i, tableName);
+                list1.Clear();
+
             }
             
             return;
         }
 
-        static void fillInTable(string nodeValue, int i, string tableName, int j)
+        static void fillInTable(List<string> list1, int i, string tableName)
         {
             string path = @"C:\Users\Юрій\Desktop\myTest\test4\test4\bin\Debug\sample.db3";
             bool fileExist = File.Exists(path);
@@ -111,19 +105,38 @@ namespace TEST4_FOR_GIT
                     }
                     cmd.ExecuteNonQuery();
 
+                    // Get the schema for the columns in the database.
+                    DataTable ColsTable = conn.GetSchema("Columns");
+
+                    // Query the columns schema using SQL statements to work out if the required columns exist.
+                    bool ColumnExists = ColsTable.Select($"COLUMN_NAME='{i}' AND TABLE_NAME='{tableName}'").Length != 0;
+
+                    if (!ColumnExists)
+                    {
+                        cmd.CommandText = $"ALTER TABLE {tableName} ADD COLUMN '{i}' NVARCHAR(2048) NULL";
+                        cmd.ExecuteNonQuery();
+                    }
 
                     // Insert entries in database table
                     if (i == 1)
                     {
-                        cmd.CommandText = $"INSERT INTO {tableName}('{i}') VALUES('{nodeValue}')";
-                        cmd.ExecuteNonQuery();
+                        for (int j = 0; j < list1.Count; j++)
+                        {
+                            cmd.CommandText = $"INSERT INTO {tableName}('{i}') VALUES('{list1[j]}')";
+                            cmd.ExecuteNonQuery();
+                            //cmd.CommandText = "SELECT LAST_INSERT_ROWID()";
+
+                        }
                     }
                     else
                     {
-                        cmd.CommandText = $"UPDATE {tableName} SET ('{i}') = ('{nodeValue}') WHERE ID = '{j}'";
-                        cmd.ExecuteNonQuery();
-
+                        for (int j = 0; j < list1.Count; j++)
+                        {
+                            cmd.CommandText = $"UPDATE {tableName} SET ('{i}') = ('{list1[j]}') WHERE ID = '{j + 1}'";
+                            cmd.ExecuteNonQuery();
+                        }
                     }
+
                     // Select and display database entries
                     //cmd.CommandText = $"Select * FROM {tableName}";
                     //using (var rdr = cmd.ExecuteReader())
