648708fd8c922617f7289690b92b7029f08a3a97
Yurii Litvin <yurijlitviniii@gmail.com>
2020-03-09 17:30:50 +0200

    the latest version of the prog. I need Telegram Bot

--- TEST4 FOR GIT/Program.cs
+++ TEST4 FOR GIT/Program.cs
@@ -7,11 +7,15 @@ using HtmlAgilityPack;
 using System.Data.SQLite;
 using System.IO;
 using System.Data;
+using System.Timers;
+using Telegram.Bot;
 
 namespace TEST4_FOR_GIT
 {
     class Program
     {
+        private static readonly TelegramBotClient Bot = new TelegramBotClient("1028340877:AAGZMZOwKrdZD5-yrONAlgv4Tmlytk6ShiA");
+
         const string createQuery = @"CREATE TABLE IF NOT EXISTS
                                 [UkrNet] (
                                 [ID] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -27,23 +31,44 @@ namespace TEST4_FOR_GIT
 
         static void Main(string[] args)
         {
+            Timer aTimer = new Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 30000;
+            aTimer.Enabled = true;
+            Console.WriteLine("Press \'q\' to quit the sample.");
+            while (Console.Read() != 'q') ;
+            //TelegramBot();
+
+
+
+        }
+
+
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            CallParser();
+
+        }
+        static void CallParser()
+        {
             Parser("http://www.ukr.net", "//article//section", "/*[position()<last()]//a", "UkrNet");
             //Parser("http://www.ukr-online.com", "//td[1]/div[1]/div[@class ='lastblock']", "//a", "UkrOnline");
-
-            //Console.ReadKey();
         }
+
+
 
         public static void Parser(string url, string nodeCount, string nodeSelect, string tableName)
         {
             HtmlWeb web = new HtmlWeb();
             HtmlDocument doc = web.Load(url);
             //
-            List<string> list1 = new List<string>();
+            List<string> listSite = new List<string>();
 
             // determines amount of parse regions/SQLite columns
 
-            //int count = doc.DocumentNode.SelectNodes(nodeCount).Count;!!!!!!
-            for (int i = 1; i <= 1; i++) //count!!!!!!
+            int count = doc.DocumentNode.SelectNodes(nodeCount).Count;
+            for (int i = 1; i <= count; i++)
             {
                 //indicator for each parsed region
                 Console.WriteLine($"[{i}]");
@@ -55,16 +80,16 @@ namespace TEST4_FOR_GIT
                 foreach (var node in htmlNodes)
                 {
                     string nodeValue = node.Attributes["href"].Value;
-                    list1.Add(nodeValue);
+                    listSite.Add(nodeValue);
                 }
-                FillInTable(list1, i, tableName);
-                list1.Clear();
+                FillInTable(listSite, i, tableName);
+                listSite.Clear();
             }
         }
 
-        static void FillInTable(List<string> list1, int i, string tableName)
+        static void FillInTable(List<string> listSite, int i, string tableName)
         {
-            List<string> list = new List<string>();
+            List<string> listDB = new List<string>();
             string path = @"C:\Users\Юрій\Desktop\for check\TEST4 FOR GIT\TEST4 FOR GIT\bin\Debug\sample.db3";
             bool fileExist = File.Exists(path);
             if (!fileExist)
@@ -101,72 +126,87 @@ namespace TEST4_FOR_GIT
                         cmd.CommandText = $"ALTER TABLE {tableName} ADD COLUMN '{i}' NVARCHAR(2048) NULL";
                         cmd.ExecuteNonQuery();
                     }
-                    // insert here reading the table column and compare with "list1"
-                    // than resultList insert to the table
-                    using (var rdr = cmd.ExecuteReader())
-                    {
-
-                        while (rdr.Read())
-                        {
-                            string readerLine = rdr.GetString(i);
-                            list.Add(readerLine);
-                        }
-                        //Console.WriteLine(list.Count);
-                        //Console.ReadLine();
-                    }
-
-                    var listResult = list1.Except(list).ToList();
-                    if (listResult.Count != 0)
-                    {
-                        Console.WriteLine("Here we have new lines!!!");
-                        Console.WriteLine(listResult.Count);
-                        Console.ReadLine();
-                    }
                     else
-
                     {
-                        Console.WriteLine("There are no new lines");
-                        Console.ReadLine();
+                        // insert here reading the table column and compare with "list1"
+                        // than resultList insert to the table
+                        cmd.CommandText = $"Select * FROM {tableName}";
+                        using (var rdr = cmd.ExecuteReader())
+                        {
+                            while (rdr.Read())
+                            {
+                                if (rdr[$"{i}"] != DBNull.Value)
+                                //if (rdr.GetString(i).GetType() != typeof(DBNull))
+                                {
+                                    string readerLine = (rdr[$"{i}"]).ToString();
+                                    //string readerLine = rdr.GetString(i);
+                                    listDB.Add(readerLine);
+                                }
+                                //else
+                                //{
+                                //    string readerLine = rdr.GetString(i);
+                                //    listDB.Add(readerLine);
+                                //}
+
+
+                            }
+                            //Console.WriteLine(list.Count);
+                            //Console.ReadLine();
+                        }
                     }
+                    var listResult = listSite.Except(listDB).ToList();
+                    //if (listResult.Count != 0)
+                    //{
+                    //    Console.WriteLine("Here we have new lines!!!");
+                    //    Console.WriteLine(listResult.Count);
+                    //    Console.ReadLine();
+                    //}
+                    //else
+                    //{
+                    //    Console.WriteLine("There are no new lines");
+                    //    Console.ReadLine();
+                    //}
 
 
 
                     // Insert entries in database table
                     cmd.CommandText = $"SELECT COUNT(1) FROM {tableName}";
                     var rowsCount = Convert.ToInt32(cmd.ExecuteScalar());
-                    int rows = rowsCount - list1.Count;
+                    int rows = rowsCount - listResult.Count;
 
 
                     if (i == 1)
                     {
-                        for (int j = 0; j < list1.Count; j++)
+                        for (int j = 0; j < listResult.Count; j++)
                         {
-                            cmd.CommandText = $"INSERT INTO {tableName}('{i}') VALUES('{list1[j]}')";
+                            cmd.CommandText = $"INSERT INTO {tableName}('{i}') VALUES('{listResult[j]}')";
                             cmd.ExecuteNonQuery();
                         }
                     }
                     else
                     {
-                        for (int j = 0; j < list1.Count; j++)
+                        for (int j = 0; j < listResult.Count; j++)
                         {
-                            cmd.CommandText = $"UPDATE {tableName} SET ('{i}') = ('{list1[j]}') WHERE ID = '{rows + j + 1}'";
+                            cmd.CommandText = $"UPDATE {tableName} SET ('{i}') = ('{listResult[j]}') WHERE ID = '{rows + j + 1}'";
                             cmd.ExecuteNonQuery();
                         }
                     }
-
-                    // Select and display database entries
-
-                    cmd.CommandText = $"Select * FROM {tableName}";
-
-
-                    
+                    listResult.Clear();
                     // Close the connection to the database
                     conn.Close();
                 }
             }
-
-            
         }
+        static async void TelegramBot()
+        {
+
+            ////var me = botClient.GetMeAsync().Result;
+            //// Console.WriteLine("Hello, World! I am user  and my name is Yurii))).");
+            //await botClient.SendTextMessageAsync("@FreshNewsUkraine", text: "Hello, World!!!)))");
+
+        }
+
+
 
 
 
