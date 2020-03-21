c7e8dd2b9f177ab41d629fcb0a73e19a07366637
Sergey Zhizhko <Sergey.Zhizhko@dataart.com>
2020-03-21 21:09:20 +0200

    fix output

--- TEST4 FOR GIT/Program.cs
+++ TEST4 FOR GIT/Program.cs
@@ -71,15 +71,14 @@ namespace TEST4_FOR_GIT
             Parser UkrNetParser = new Parser("http://www.ukr.net", "//article//section", "*[position()<last()]//a");
             //Parser UkrOnline = new Parser("http://www.ukr-online.com", "//td[1]/div[1]/div[@class ='lastblock']", "a");
             
-            foreach (var chapter in UkrNetParser.GetNews())
+            foreach (KeyValuePair<string, List<Article>> chapterArticlesPair in UkrNetParser.GetNews())
             {
-                Console.WriteLine(chapter.Key);
+                Console.WriteLine(chapterArticlesPair.Key);
 
-                //foreach (List<Article> article in chapter)
-                //{
-                //    // TODO: use already created item.GetArticleTextWithUrl() or create something similar
-                //    Console.WriteLine(item);
-                //}
+                foreach (Article article in chapterArticlesPair.Value)
+                {
+                    Console.WriteLine(article.GetArticleTextWithUrl());
+                }
             }
             Console.ReadLine();
 
