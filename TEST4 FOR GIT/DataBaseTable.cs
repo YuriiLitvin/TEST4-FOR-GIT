175c845da067023eb9cd52135982e086bd5f6288
Yurii Litvin <yurijlitviniii@gmail.com>
2020-03-22 21:01:02 +0200

    last changes to look at 21:00

--- TEST4 FOR GIT/DataBaseTable.cs
+++ TEST4 FOR GIT/DataBaseTable.cs
@@ -10,17 +10,25 @@ namespace TEST4_FOR_GIT
 {
     class DataBaseTable : DataBase
     {
-        public string SiteName { get; set; }
+        public string TableName { get; set; }
+
+        public DataBaseTable(string Url) 
+        {
+            TableName = Url.Substring(11);
+        }
+
+
         
-        const string createQuery = $@"CREATE TABLE IF NOT EXISTS
-                                [{SiteName}] (
+
+        public void CreateDBTable()
+        {   
+            string createQuery = $@"CREATE TABLE IF NOT EXISTS
+                                [{TableName}] (
                                 [ID] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                                 [1] NVARCHAR (MAX) NULL)";
 
-        
 
-        public void CreateDataBaseTable()
-        {
+
             using (var conn = new SQLiteConnection("data source = sample.db3"))
             {
                 //create a database command 
@@ -37,75 +45,54 @@ namespace TEST4_FOR_GIT
             }
 
         }
-        //// Get the schema for the columns in the database.
-        //DataTable ColsTable = conn.GetSchema("Columns");
 
-        //// Query the columns schema using SQL statements to work out if the required columns exist.
-        //bool ColumnExists = ColsTable.Select($"COLUMN_NAME='{i}' AND TABLE_NAME='{tableName}'").Length != 0;
-
-        //if (!ColumnExists)
-        //{
-        //    cmd.CommandText = $"ALTER TABLE {tableName} ADD COLUMN '{i}' NVARCHAR(2048) NULL";
-        //    cmd.ExecuteNonQuery();
-        //}
-        //else
-        //{
-        // reading the table column and compare with listSite
-        // than resultList insert to the table
-
-
-        public Dictionary<string,List<Article>> ReadDataBaseTable() 
+        public Dictionary<string,List<Article>> ReadDBTable() 
         {
-            var ParserDB = new Dictionary<string, List<Article>>();
-            cmd.CommandText = $"Select * FROM {SiteName}";
+            var DictionaryDB = new Dictionary<string, List<Article>>();
+            
+            //NOTE: here I have to use "using" operator
+            cmd.CommandText = $"Select * FROM {TableName}";
             using (var rdr = cmd.ExecuteReader())
             {
                 while (rdr.Read())
                 {
                     if (rdr["1"] != DBNull.Value)
                     {
-                                   
-                        //string readerLine = (rdr[$"{i}"]).ToString();
-                        //listDB.Add(readerLine);
+                         DictionaryDB = (rdr["1"]);
                     }
-
                 }
-
             }
-            return ParserDB;
+            return DictionaryDB;
         }
 
 
-        public void GetNewsDifference()
+        public Dictionary<string, List<Article>> GetNewsDifference()
         {
-            var listResult = listSite.Except(listDB).ToList();
+            var DictionaryDifference = new Dictionary<string, List<Article>>();
+            //NOTE: here I want to compare just List<Article> of new and previous dictionaries 
+            //while the keyValues are equal
+            // I think to use two times "if
+            foreach (KeyValuePair<string, List<Article>> chapterArticlesPair in news)
+            {
+                Console.WriteLine(chapterArticlesPair.Key);
+
+                foreach (Article article in chapterArticlesPair.Value)
+                {
+                    //var listResult = listSite.Except(listDB).ToList();    
+                }
+            }
+
+
+            return DictionaryDifference;
+            
         }
 
-
+        //NOTE: here I will replace "void" with Dictionary<string,List<Artilce>>
         public void SaveNewsdifference()
         {
             // Insert entries in database table
-            cmd.CommandText = $"SELECT COUNT(1) FROM {tableName}";
-            var rowsCount = Convert.ToInt32(cmd.ExecuteScalar());
-            int rows = rowsCount - listResult.Count;
-
-
-            if (i == 1)
-            {
-                for (int j = 0; j < listResult.Count; j++)
-                {
-                    cmd.CommandText = $"INSERT INTO {tableName}('{i}') VALUES('{listResult[j]}')";
-                    cmd.ExecuteNonQuery();
-                }
-            }
-            else
-            {
-                for (int j = 0; j < listResult.Count; j++)
-                {
-                    cmd.CommandText = $"UPDATE {tableName} SET ('{i}') = ('{listResult[j]}') WHERE ID = '{rows + j + 1}'";
-                    cmd.ExecuteNonQuery();
-                }
-            }
+            cmd.CommandText = $"INSERT INTO {TableName}('1') VALUES('{listResult[j]}')";
+            cmd.ExecuteNonQuery();
             listResult.Clear();
         }
     
